0f9d4c9249955c437f2b408ec170e0c07d3b51e5
gtownbandit <kasper.philip@outlook.com>
2025-07-21 21:45:22 +0200

    prisma import to supabase worked

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
