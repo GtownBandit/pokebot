4bd500730747f91af2a3888e061256024bb499a4
gtownbandit <kasper.philip@outlook.com>
2025-07-22 20:46:30 +0200

    bugfixing vercel envs

--- apps/frontend/scripts/set-env.mjs
+++ apps/frontend/scripts/set-env.mjs
@@ -1,7 +1,7 @@
 import { writeFileSync } from 'fs';
 import { join } from 'path';
 
-const targetPath = join('./src/environments/environment.prod.ts');
+const targetPath = join('../src/environments/environment.prod.ts');
 
 const envConfig = `
 export const environment = {
@@ -13,4 +13,5 @@ export const environment = {
 `;
 
 console.log('Writing environment.prod.ts with Vercel environment variables...');
+console.log(envConfig)
 writeFileSync(targetPath, envConfig);
