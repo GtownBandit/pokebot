0520aca4b75b9e0576a82665d5d53bd851bc1e05
gtownbandit <kasper.philip@outlook.com>
2025-07-25 23:05:35 +0200

    allow channel owner to spawn events

--- apps/backend/src/chatbot/twitch.service.ts
+++ apps/backend/src/chatbot/twitch.service.ts
@@ -87,7 +87,7 @@ export class TwitchService implements OnModuleInit {
         this.pokemonCatchService.tryCatchPokemon(user);
       } else if (
         text.trim().toLowerCase() === '!spawn' &&
-        user.toLowerCase() === process.env.TWITCH_BOT_USERNAME
+        user.toLowerCase() === process.env.TWITCH_CHANNEL
       ) {
         this.pokemonSpawnService.spawnRandomPokemon();
       }
