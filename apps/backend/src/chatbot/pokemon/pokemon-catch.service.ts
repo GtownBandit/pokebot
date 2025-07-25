cff02627d467457ca1f3a508594d03830b43863e
gtownbandit <kasper.philip@outlook.com>
2025-07-25 22:58:24 +0200

    bugfix for catching expired pokemon

--- apps/backend/src/chatbot/pokemon/pokemon-catch.service.ts
+++ apps/backend/src/chatbot/pokemon/pokemon-catch.service.ts
@@ -1,11 +1,11 @@
 // apps/backend/src/chatbot/pokemon/pokemon-catch.service.ts
-import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../prisma/prisma.service';
 import { TwitchService } from '../twitch.service';
 
 @Injectable()
 export class PokemonCatchService {
-  private readonly logger = new Logger(PokemonCatchService.name);
+  // private readonly logger = new Logger(PokemonCatchService.name);
 
   constructor(
     private prisma: PrismaService,
@@ -14,6 +14,12 @@ export class PokemonCatchService {
   ) {}
 
   async tryCatchPokemon(username: string) {
+    // Find or create user by username
+    let user = await this.prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      user = await this.prisma.user.create({ data: { username } });
+    }
+
     // Get the current active spawn event
     const spawnEvent = await this.prisma.spawnEvent.findFirst({
       where: {
@@ -35,12 +41,6 @@ export class PokemonCatchService {
     const roll = Math.floor(Math.random() * 100) + 1;
     const level = spawnEvent.level;
     const pokemonName = spawnEvent.pokemon.displayNameDe;
-    // Find or create user by username
-    let user = await this.prisma.user.findUnique({ where: { username } });
-    if (!user) {
-      user = await this.prisma.user.create({ data: { username } });
-    }
-
     // Create catch roll event with userId
     await this.prisma.catchRollEvent.create({
       data: {
@@ -63,8 +63,9 @@ export class PokemonCatchService {
     } else {
       const escapeRoll = Math.floor(Math.random() * 10) + 1;
       if (escapeRoll <= 1) {
-        await this.prisma.spawnEvent.delete({
+        await this.prisma.spawnEvent.update({
           where: { id: spawnEvent.id },
+          data: { expiresAt: new Date() },
         });
         await this.twitchService.sendChatMessage(
           `${pokemonName} ist entkommen! (Wurf: ${roll} / Level: ${level})`,
