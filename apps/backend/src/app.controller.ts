9bf96676a6e19ee39f653f6624101803bdc735d9
gtownbandit <kasper.philip@outlook.com>
2025-07-26 11:25:02 +0200

    added open getters (maybe one day secure those?)

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,16 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
-  // @Get('users')
-  // async getUsers() {
-  //   return await this.prisma.user.findMany();
-  // }
-  // @Get('pokemon')
-  // async getPokemon() {
-  //   return await this.prisma.pokemonSpecies.findMany();
-  // }
+  @Get('users')
+  async getUsers() {
+    return await this.prisma.user.findMany();
+  }
+  @Get('pokemon')
+  async getPokemon() {
+    return await this.prisma.pokemon.findMany();
+  }
+  @Get('pokemon-instances')
+  async getPokemonInstances() {
+    return await this.prisma.pokemonInstance.findMany();
+  }
+  @Get('catch-roll-events')
+  async getCatchRolls() {
+    return await this.prisma.catchRollEvent.findMany();
+  }
+  @Get('spawn-events')
+  async getSpawnEvents() {
+    return await this.prisma.catchRollEvent.findMany();
+  }
 }
