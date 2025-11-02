f8ec69be206b29d42510fbe63fa54ee5b09b04c6
gtownbandit <kasper.philip@outlook.com>
2025-11-02 17:28:32 +0100

    add .dockerignore, update Dockerfile and docker-compose.yaml, and configure environment variables

--- apps/frontend/src/app/features/login/login.component.ts
+++ apps/frontend/src/app/features/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { AuthService as Auth0Service } from '@auth0/auth0-angular';
 import { LoginBackgroundComponent } from '../login-background/login-background.component';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,7 @@ export class LoginComponent {
   private auth = inject(Auth0Service);
 
   login() {
+    console.log(JSON.stringify(environment));
     this.auth.loginWithRedirect();
   }
 }
--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  authDomain: '',
-  authClientId: '',
-  authAudience: '',
-  backendURL: '',
-  frontendURL: '',
+  authDomain: 'https://auth.pokebot.at',
+  authClientId: 'YtVam2xqYB7b5VX3sJLHMiILFrB7DJRK',
+  authAudience: 'https://api.pokebot.at',
+  backendURL: 'https://api.pokebot.at',
+  frontendURL: 'https://pokebot.at',
 };
